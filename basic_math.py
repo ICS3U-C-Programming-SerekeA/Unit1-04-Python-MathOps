845a7f6e64ae2433eeff9e6702e08f62fc7b825b
Sereke Asfeday <sereke.asfeday@stu.ocsb.ca>
2026-09-21 23:30:07 +0000

    First commit of basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Sereke Asfeday
+# Date: Sep. 21th, 2026
+# This program displays a serious of math equations.
+def main():
+    print("2 + 3 = {}".format(2 + 3))
+    print("6 - 3 = {}".format(6 - 3))
+    # In multiplaying x is used for output Q while the * is the actual multiplication sign.
+    print("6 * 3 = {}".format(6 * 3))
+    print("7 / 2 = {}".format(7 / 2))
+    print("2 ^ 3 = {}".format(2**3))  # This is exponent.
+    print("16 ** 0.5 = {}".format(16**0.5))  # This is square root.
+    print(
+        "1+3*4 = {}".format(1 + 3 * 4)
+    )  # BEDMAS rule is used here. Multiplication is done first then addition.
+    print("10 / 5 = {}".format(10 / 5))  # This is division.
+
+
+if __name__ == "__main__":
+    main()
